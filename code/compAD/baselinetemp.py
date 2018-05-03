66a955f0ec7a5bd9321443c9b0b3eda6bc80062a
wtfenfenmiao <wtbupt@outlook.com>
2018-05-03 17:47:48 +0800

    这个代码非常吃内存，如果组里服务器不行，就得优化内存了，现在连个del gc.collect都没有......

--- code/compAD/baselinetemp.py
+++ code/compAD/baselinetemp.py
@@ -4,6 +4,7 @@ import pandas as pd
 import lightgbm as lgb
 from sklearn.preprocessing import OneHotEncoder,LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from scipy import sparse
 """
 1.弄三个csv的demo，用来debug用
@@ -141,6 +142,22 @@ def fillNA_data():
     mergedData.to_csv(data_path+"mergedDataFillna.csv",index=False)
 
 
+
+# 这个函数没啥用，只是因为下一个函数漏写了......
+# 还有测试功能
+def uid_aid_out():
+    data = pd.read_csv(data_path + "mergedDataFillna.csv",nrows=300)
+    train = data[data.label != -1]
+    print (train.size)
+    train_y = train.pop('label')  # pop的作用，在train里删除label这一列，并且返回label这一列
+    print (train_y.size)
+    print (train_y)
+    train_y.to_csv(data_path + "train_y_test.csv", index=False)
+    train_y=pd.read_csv(data_path+"train_y_test.csv",header=None)    #这个header=None一定要加！否则没有列名的时候，第一行是数据，不加这个header=None就会把第一行变成列名，然后少一行！
+    print (train_y)
+    print (train_y.size)
+
+
 def data_final():
     data=pd.read_csv(data_path+"mergedDataFillna.csv")
     #print (data)
@@ -197,13 +214,63 @@ def data_final():
     sparse.save_npz(data_path+"train_x.npz",train_x)
     sparse.save_npz(data_path + "test_x.npz", test_x)
     train_y.to_csv(data_path+"train_y.csv",index=False)
+    res.to_csv(data_path + "res.csv", index=False)
     #tee=sparse.load_npz(data_path+"train_x.npz")
     #print (tee)
 
 
 
 
+#这个用法是skicit-learn和lightgbm的版本。去lightgbm中文手册里找，scikit-learn API
+def lightgbm_train_predict(train_x,train_y,test_x,res):
+    clf=lgb.LGBMClassifier(
+        boosting_type="gbdt",
+        num_leaves=31,
+        reg_alpha=0.0,       #l1正则
+        reg_lambda=1,        #l2正则
+        max_depth=-1,
+        n_estimators=1500,    #决策树的最大的数量
+        objective="binary",
+        subsample=0.7,     #随机选数据，防止过拟合
+        colsample_bytree=0.7,    #随机选特征，防止过拟合
+        subsample_freq=1,
+        learning_rate=0.05,
+        min_child_weight=50,
+        random_state=2018,
+        n_jobs=100
+    )
+    print ("parameter prepared!")
+    #原来的没用没用交叉验证集，用train训练，用train做交叉验证......虽然我觉得这样不太好......
 
+    # #划交叉验证集，线下测试来定参数,然后交叉验证集定好参数之后，训练模型要用所有的train训练，这个机器学习基石里说过......
+    # train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.2)
+    # print ("validation prepared!")
+
+    #感觉这个early_stop_round和迭代次数两个参数完全可以只留一个......
+    #clf.fit(train_x,train_y,eval_set=[(val_x,val_y)],eval_metric='auc',early_stopping_rounds=100)
+    clf.fit(train_x, train_y, eval_set=[(train_x, train_y)], eval_metric='auc', early_stopping_rounds=100)
+    joblib.dump(clf,data_path+"train_model.m")
+    #clf=joblib.load("train_model.m)
+    print ("model finished!")
+
+    res["score"]=clf.predict_proba(test_x)[:,1]
+    res["score"]=res["score"].apply(lambda x:float("%.6f" % x))   #保留6位小数
+    res.to_csv(data_path+"submission.csv",index=False)
+    print ("finish!")
+
+
+
+def call_lightgbm():
+    #这里这个header=None就这个train_y用，就它没有列名（pop操作弄出来的），然后别的都有列名
+    #之前fit和train_test_split报错：ValueError: Found input variables with inconsistent numbers of samples:[4，3]，这种报错意思是前后没对上，你给的x是4行，但是y是3行这样子
+    train_x=sparse.load_npz(data_path + "train_x.npz")
+    train_y=pd.read_csv(data_path+"train_y.csv",header=None)
+    test_x = sparse.load_npz(data_path + "test_x.npz")
+    res=pd.read_csv(data_path+"res.csv")      #res是aid和uid
+    print ("data loaded!")
+    #print (train_x.size)
+    #print (train_y.size)
+    lightgbm_train_predict(train_x, train_y, test_x, res)
 
 
 
@@ -217,14 +284,16 @@ def data_final():
 #查看数据前50行，看样子这种
 #还有一些代码里不确定的，可以拿出来几行数据在这里当小白鼠
 def view_data():
-    temp=pd.read_csv(data_path+"mergedDataFillna.csv",nrows=300)
-    #print (temp)
-    #print (temp[['appIdAction', 'appIdInstall']])
-
-    #one-hot的代码可以输出下边的东西理解一下。那个OneHotEncoder的fit里，得放一个最后reshape(-1,1)之后出来的二维array
-    print (temp["marriageStatus"])
-    print (temp["marriageStatus"].values)
-    print (temp["marriageStatus"].values.reshape(-1,1))
+    train=pd.read_csv(data_path+"train.csv")
+    print (train.size)
+    # temp=pd.read_csv(data_path+"mergedDataFillna.csv",nrows=300)
+    # #print (temp)
+    # #print (temp[['appIdAction', 'appIdInstall']])
+    #
+    # #one-hot的代码可以输出下边的东西理解一下。那个OneHotEncoder的fit里，得放一个最后reshape(-1,1)之后出来的二维array
+    # print (temp["marriageStatus"])
+    # print (temp["marriageStatus"].values)
+    # print (temp["marriageStatus"].values.reshape(-1,1))
 
 
 #取几行看看用户特征啥德行,每次的结果都记下来，否则太大了不好弄
@@ -271,4 +340,6 @@ if __name__=="__main__":
     #train_pred_feature_concat()
     #fillNA_data()
     #view_data()
-    data_final()
+    #data_final()
+    #uid_aid_out()
+    call_lightgbm()
